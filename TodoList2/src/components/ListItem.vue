82c9906a350dfbecd31dc0bb33bc4305ef9fa55d
iyad <imoucharrafie@gmail.com>
2025-01-11 16:38:00 -0400

    first commit

---
+++ TodoList2/src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    defineProps<{
+        isChecked?: boolean;
+    }>();
+</script>
+
+<template>
+    <label :class="{'checked': isChecked}">
+      <input type="checkbox" :ischeck="isChecked" />
+      <slot></slot>
+
+    </label>
+</template>
+
+<style scoped>
+    Label {
+        cursor: pointer;
+    }
+    .checked {
+        text-decoration: line-through;
+    }
+</style>
---
+++ TodoList2/src/components/__tests__/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+
+<template>
+    <header>
+        <h1><span class="icon" aria-hidden="true">Lista de tareas</span></h1>
+        <p>Metas 2025</p>
+    </header>
+</template>
+
+
+<style scoped>
+header {
+    border-bottom: #333 solid 1px;
+    background-color: #fff;
+
+    header {
+        border-bottom:#333 solid 1px;
+        background-color: #fff;
+    }
+}
+header : :after {
+    content: "";
+    display: block;
+    height: 1px;
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+}
+h1 {
+    front-size: 2rem;
+}
+
+h1 .icon {
+    font-size: 1rem;
+    vertical-align: middle;
+}
+</style>
